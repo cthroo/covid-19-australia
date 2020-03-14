efae536a8f5729b601b1ab0742541e8709549100
Robert Chang <cht8687@gmail.com>
2020-03-15 00:38:41 +1100

    add in australia d3 map

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Australia from '../components/australia';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,7 +27,11 @@ export default function Index() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
@@ -35,7 +40,7 @@ export default function Index() {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <Australia />
     </div>
   );
 }
-
